40eb873e6a6672deaa06a204a52cc404ba9eb4f2
Sridharan <srikprcas@gmail.com>
2026-06-04 15:39:40 +0530

    Js variables,Data types and Operators done

---
+++ Javascript/script.js
@@ -0,0 +1,85 @@
+// // var a=10;
+// // let b=20;
+// // const c=30;
+
+// // console.log(a)
+// // console.log(b)
+// // console.log(c)
+
+// // var num = 10;
+// // var str ="Sridharan";
+// // var undefine;
+// // var Null = null;
+// // var boolean = true;
+ 
+// // console.log(typeof num)
+// // console.log(typeof str)
+// // console.log(typeof undefine)
+// // console.log(typeof Null)
+// // console.log(typeof boolean)
+
+// //  var bigInt = 123n;
+// //  var symbol = Symbol('li');
+
+// //  console.log(typeof bigInt)
+// //  console.log(typeof symbol)
+
+// //  var arr = [10,20,30,40];
+// //  console.log(typeof arr, arr);
+// //  var obj = {
+// //     name:"sridharan",
+// //     dept:["aids","ct"]
+// //  }
+// //  console.log(typeof obj,obj)
+
+// var a =10;
+// var b = "sri";
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a%b)
+// console.log(a/b)
+
+// var a = true;
+// var b = false;
+
+// console.log(a&&b)
+// console.log(a||b)
+// console.log(!a)
+
+// a = 10;
+// b = 20;
+
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a<=b)
+// console.log(a>=b)
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(a===b)
+
+// a = 10;
+// b = 20;
+// a += b;
+
+// console.log(a)
+// a -= b;
+// console.log(a)
+
+// var a =10;
+
+//  console.log(a++);
+//  console.log(++a);
+
+//   var a =10;
+
+//   console.log(a--);
+//   console.log(--a);
+
+  a = 1;
+  b = 1;
+  c = 0;
+
+  var result = a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+  console.log(result)
